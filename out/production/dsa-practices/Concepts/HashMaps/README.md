6dbaa0691b8b13a43172906da0ee83f8363a4b87
Musa Selekie Kanneh <musas.kanneh@gmail.com>
2021-10-12 08:46:11 +0200

    Identical number pair

--- out/production/dsa-practices/Concepts/HashMaps/README.md
+++ out/production/dsa-practices/Concepts/HashMaps/README.md
@@ -2,5 +2,5 @@
 [LeetCode](https://leetcode.com/tag/hash-table/)
 
 ## Questions and Answers
-1. [Number of Good Pairs](https://github.com/musakanneh/dsa-java/blob/main/src/HashMaps/Easy/NumberIdenticalPairs.java)
+1. [Number of Good Pairs](https://github.com/musakanneh/dsa-java/blob/main/src/Concepts/HashMaps/Easy/NumberIdenticalPairs.java)
 2. 
--- src/Concepts/HashMaps/Easy/NumberIdenticalPairs.java
+++ src/Concepts/HashMaps/Easy/NumberIdenticalPairs.java
@@ -1,26 +1,18 @@
 package Concepts.HashMaps.Easy;
-import java.util.HashMap;
+import java.util.*;
 
 /**
  * Given an array of integers nums, return the number of good pairs.
  * A pair (i, j) is called good if nums[i] == nums[j] and i < j.
  *
- * Example 1:
- *  Input: nums = [1,2,3,1,1,3]
- *  Output: 4
- *  Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
+ * https://leetcode.com/problems/number-of-good-pairs/
  */
-
 public class NumberIdenticalPairs {
     public static void main(String[] args) {
-        System.out.println(numIdenticalPairs1());
-        System.out.println(numIdenticalPairs2());
+        System.out.println(numIdenticalPairs());
     }
 
     public static int numIdenticalPairs1() {
-        /**
-         * brute force solution: (n - 1) * (n) / 2
-         */
         int [] nums = {1,2,3,1,1,3};
         int good_pair = 0;
 
@@ -35,15 +27,8 @@ public class NumberIdenticalPairs {
     }
 
     public static int numIdenticalPairs2() {
-        /**
-         * combinatorics approach: (n - 1) * (n) / 2
-         *
-         * Indicate req limit using an array
-         * traverse the array and track elements' req
-         * apply a combinatorics approach to items with > 1 freq
-         */
-        int [] nums = {1,2,3,1,1,3};
         int MAX_LEN = 101;
+        int [] nums = {1,2,3,1,1,3};
         int good_pairs = 0;
         int [] freq = new int[MAX_LEN];
 
@@ -58,16 +43,22 @@ public class NumberIdenticalPairs {
         return good_pairs;
     }
 
-    public static HashMap<Integer, Integer> numIdenticalPairs() {
-        /**
-         * Solution using hashmap
-         */
-        int [] nums = {1,2,3,1,1,3};
+    public static int numIdenticalPairs() {
+        int [] nums = {1, 1, 1, 1};
+        int freq_count = 0;
+
         HashMap<Integer, Integer> good_pairs = new HashMap<>();
 
-        for (int num : nums) {
-//            if (num == good_pairs)
+        for (int i = 0; i < nums.length; i++) {
+            int element_at_index = nums[i];
+
+            if (good_pairs.containsKey(element_at_index)) {
+                freq_count += good_pairs.get(element_at_index);
+                good_pairs.put(element_at_index, good_pairs.get(element_at_index) + 1);
+            } else {
+                good_pairs.put(element_at_index, 1);
+            }
         }
-        return good_pairs;
+        return freq_count;
     }
 }
