328076645116bfe70edfb71b315c588592a09723
Musa Selekie Kanneh <musas.kanneh@gmail.com>
2021-10-19 20:25:45 +0200

    Climbing stairs

---
+++ src/Easy/String/ClimbStairs.java
@@ -0,0 +1,30 @@
+package Easy.String;
+
+/**
+ * You are climbing a staircase. It takes n steps to reach the top.
+ *
+ * Each time you can either climb 1 or 2 steps.
+ * In how many distinct ways can you climb to the top?
+ *
+ * https://leetcode.com/problems/climbing-stairs/
+ */
+public class ClimbStairs {
+    public static void main(String[] args) {
+        System.out.println(climbStairs(2));
+        System.out.println(climbStairs(3));
+        System.out.println(climbStairs(4));
+        System.out.println(climbStairs(5));
+    }
+
+    public static int climbStairs(int n) {
+        if (n == 1) return n;
+
+        int [] step_count = new int[n + 1];
+        step_count[1] = 1; step_count[2] = 2;
+        
+        for (int i = 3; i <= n; i++) {
+            step_count[i] = step_count[i - 1] + step_count[i - 2];
+        }
+        return step_count[n];
+    }
+}
