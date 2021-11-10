299f0ad46a1b36bf6dca7c4f9a7dc1b92a2fe9ae
Musa Selekie Kanneh <musas.kanneh@gmail.com>
2021-11-10 18:42:53 +0200

    Updates

--- src/Easy/TwoPointers/DiStringMatch.java
+++ src/Easy/TwoPointers/DiStringMatch.java
@@ -1,5 +1,7 @@
 package Easy.TwoPointers;
 
+import java.util.Arrays;
+
 /**
  * A permutation perm of n + 1 integers of all the integers in the range [0, n]
  * can be represented as a string s of length n where:
@@ -12,4 +14,23 @@ package Easy.TwoPointers;
  * https://leetcode.com/problems/di-string-match/
  */
 public class DiStringMatch {
+    public static void main(String[] args) {
+        System.out.println(diStringMatch("IDID"));
+    }
+
+    static int[] diStringMatch(String s) {
+        int size = s.length();
+        int low = 0; int high = size;
+        int [] result = new int[size + 1];
+
+        for (int i = 0; i < size; i++) {
+            if (s.charAt(i) == 'I') {
+                result[i] = low++;
+            } else {
+                result[i] = high--;
+            }
+        }
+        result[size] = high;
+        return result;
+    }
 }
