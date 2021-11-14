c93703d64037cbf96b6e1844cc8b9493447e3d55
Musa Selekie Kanneh <musas.kanneh@gmail.com>
2021-11-14 13:17:56 +0200

    Updates

--- src/Easy/TwoPointers/Solved/ReversePrefix.java
+++ src/Easy/TwoPointers/Solved/ReversePrefix.java
@@ -1,4 +1,4 @@
-package Easy.TwoPointers;
+package Easy.TwoPointers.Solved;
 
 /**
  * Given a 0-indexed string word and a character ch, reverse the segment of word that
@@ -12,9 +12,25 @@ package Easy.TwoPointers;
  *
  * https://leetcode.com/problems/reverse-prefix-of-word/
  */
-
 public class ReversePrefix {
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println(reversePrefix("abcdefd", 'd'));
+    }
+
+    static String reversePrefix(String word, char ch) {
+        StringBuilder string_builder = new StringBuilder();
+        boolean word_found = false; int  i = 0;
+
+        while (i < word.length()) {
+            string_builder.append(word.charAt(i));
+
+            if (word.charAt(i) == ch && !word_found) {
+                string_builder.reverse();
+                word_found = true;
+            }
+            i++;
+        }
+
+        return string_builder.toString();
     }
 }
