8b3214bcec2f1ee437c3216d0113a57c81df2cc6
Musa Selekie Kanneh <musas.kanneh@gmail.com>
2021-11-15 09:47:05 +0200

    Updates

--- src/Easy/TwoPointers/ReverseVowels.java
+++ src/Easy/TwoPointers/ReverseVowels.java
@@ -1,5 +1,8 @@
 package Easy.TwoPointers;
 
+import java.util.Arrays;
+import java.util.HashSet;
+
 /***
  * Given a string s, reverse only all the vowels in the string and return it.
  *
@@ -7,14 +10,35 @@ package Easy.TwoPointers;
  *
  * https://leetcode.com/problems/reverse-vowels-of-a-string/
  */
-
 public class ReverseVowels {
 
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println(reverseVowels("hello"));
     }
 
-    public String reverseVowels(String s) {
-        return s;
+    static String reverseVowels(String s) {
+        HashSet<Character> vowels = new HashSet<>();
+        vowels.add('A'); vowels.add('a');
+        vowels.add('E'); vowels.add('e');
+        vowels.add('I'); vowels.add('i');
+        vowels.add('O'); vowels.add('o');
+        vowels.add('U'); vowels.add('u');
+
+        char [] chars = s.toCharArray();
+        int left = 0, right = s.length() - 1;
+
+        while (left < right) {
+            while (left < right && !vowels.contains(chars[left])) {
+                left++;
+            }
+            while (left < right && !vowels.contains(chars[right])) {
+                right--;
+            }
+            char temp = chars[left];
+            chars[left++] = chars[right];
+            chars[right--] = temp;
+        }
+
+        return new String(chars);
     }
 }
