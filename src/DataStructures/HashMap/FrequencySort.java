3862a889c8449b5a8036820b158de97da864cc17
Musa Selekie Kanneh <musas.kanneh@gmail.com>
2021-11-22 12:50:43 +0200

    Added more tasks

--- src/DataStructures/HashMap/FrequencySort.java
+++ src/DataStructures/HashMap/FrequencySort.java
@@ -1,5 +1,9 @@
 package DataStructures.HashMap;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 /***
  * Given an array of integers nums, sort the array in increasing order
  * based on the frequency of the values. If multiple values have the same
@@ -11,10 +15,36 @@ package DataStructures.HashMap;
  */
 public class FrequencySort {
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println(frequencySort());
     }
 
-    static int[] frequencySort(int[] nums) {
+    static int[] frequencySort() {
+        int[] nums = {1, 1, 2, 2, 2, 3};
+        HashMap<Integer, Integer> map = new HashMap<>(); int index = 0;
 
+        for (int  num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        while (!map.isEmpty()) {
+            int min_value  = 1000, k = 0, current_value = 0;
+
+            for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+                current_value = entry.getValue();
+                
+                if (current_value < min_value) {
+                    k = entry.getKey(); min_value = current_value;
+
+                } else if (current_value == min_value) {
+                    if (k < entry.getKey()) k = entry.getKey();
+                }
+            }
+            while (min_value != 0) {
+                nums[index++] = k;
+                min_value--;
+            }
+            map.remove(k);
+        }
+
+        return nums;
     }
 }
